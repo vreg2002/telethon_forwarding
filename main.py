19aaee0d36d90310fc925667c1359c90f2915a1e
vreg2002 <91435931+vreg2002@users.noreply.github.com>
2021-09-27 01:57:17 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from telethon import TelegramClient, sync, events
 INPUT_CHANNEL = 'input_channel_username'
 OUTPUT_CHANNEL = 'output_channel_username'
 TAGS = ['#TAG1', '#TAG2']
-
+import telebot
+bot = telebot.TeleBot("2031762379:AAF6vEaCKc_yUOVMuJ3OQRRM2I5hIryzEps")
 # 1. Заходим на сайт https://my.telegram.org/apps
 # 2. Заполняем поля App title и Short name, нажимаем «Create application» и запоминаем две переменные: api_id и api_hash.
 
